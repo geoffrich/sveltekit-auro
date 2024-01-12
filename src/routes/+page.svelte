42209b1ea282671b4799715e7fbe662ab7cef10f
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2024-01-11 16:47:25 -0800

    Implement demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	// this custom element is safe to import on server & client
+	// it uses a newer version of lit that doesn't reference `window` at the top level of a script
+	import './test-component';
+	onMount(() => {
+		// this component relies on an old version of lit that references `window` directly
+		// we need to import it inside `onMount`, so that it's only imported on the client (where window is available)
+		import('@aurodesignsystem/auro-button');
+	});
+</script>
+
+<auro-button>Hello World</auro-button>
+<simple-greeting></simple-greeting>
